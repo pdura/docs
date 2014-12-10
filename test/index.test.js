50070c06ee3da6d205f2f031272eb23607cf41a3
José F. Romaniello <jfromaniello@gmail.com>
2014-12-10 16:31:01 -0300

    fix broken test

--- test/index.test.js
+++ test/index.test.js
@@ -2,20 +2,15 @@
  * Module dependencies.
  */
 
-var http = require('http');
 var request = require('supertest');
 var docsapp = require('../app');
 
-
 describe('Application', function() {
-
   describe('GET /test', function(){
     it('should respond OK with json', function(done){
       request(docsapp._app)
         .get('/test')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
         .expect(200, done);
-    })
-  })
+    });
+  });
 });
