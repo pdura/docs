c1e16e84f47d23916170363d58e1de0968ccc405
Sandrino Di Mattia <sandrinodimattia@Sandrinos-MacBook-Pro.local>
2014-12-21 00:56:52 +0100

    Fix redirect

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,7 @@
 
 var request = require('supertest');
 var docsapp = require('../app');
+var nconf = require('nconf');
 
 describe('Application', function() {
   describe('GET /test', function(){
@@ -11,6 +12,15 @@ describe('Application', function() {
       request(docsapp._app)
         .get('/test')
         .expect(200, done);
+    });
+  });
+
+  describe('GET /salesforceapi-tutorial', function(){
+    it('should respond REDIRECT with Location', function(done){
+      request(docsapp._app)
+        .get(nconf.get('BASE_URL') + '/salesforceapi-tutorial')
+  		.expect('Location', nconf.get('BASE_URL') + '/server-apis/salesforce')	
+        .expect(301, done);
     });
   });
 });
